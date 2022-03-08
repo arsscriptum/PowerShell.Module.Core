5c9cafd421acc492d33c34cd08d056cae7b84ec4
Guillaume Plante <guillaumeplante.qc@gmail.com>
2022-03-07 19:42:32 -0500

    automatic-commit

--- src/Miscellaneous.ps1
+++ src/Miscellaneous.ps1
@@ -16,14 +16,32 @@ function Get-FunctionSource{
         [Parameter(Mandatory=$true, ValueFromPipeline=$true, Position=0, HelpMessage="Function Name")]
         [string]$Name
     ) 
+    $IsFunction = $True
     $CmdType = Get-Command $Name
     if($CmdType -eq $Null){ return }
     $CmdType = (Get-Command $Name).CommandType
-    $Script = (Get-Item function:$Name).ScriptBlock
-    write-host -n -f DarkYellow "Function Name : " ;
-    write-host -f DarkRed "$Name" ;
+    $Script = ""
+    try{
+        $Script = (Get-Item function:$Name -ErrorAction Stop).ScriptBlock
+    }
+    catch{
+        $IsFunction = $False
+    }
+    
     write-host -n -f DarkYellow "Command Type  : " ;
     write-host -f DarkRed "$CmdType" ;
+
+    if(($IsFunction -eq $False)-Or($CmdType -eq 'Alias')){
+        $AliasInfo = (Get-Alias $Name).DisplayName
+        write-host -n -f DarkYellow "Alias Info : " ;
+        write-host -f DarkRed "$AliasInfo" ;     
+        $AliasDesc = (Get-Alias $Name).Description
+        write-host -n -f DarkYellow "Alias Desc : " ;
+        write-host -f DarkRed "$AliasDesc" ;   
+    }else{
+        write-host -n -f DarkYellow "Function Name : " ;
+        write-host -f DarkRed "$Name" ;
+    }
 
     return $Script
 
